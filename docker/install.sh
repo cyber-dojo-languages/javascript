6d169e9ed25c1d096ca7f162d7081f98bf9c4c53
Jon Jagger <jon@jaggersoft.com>
2020-07-17 12:32:37 +0100

    Switch to smaller Alpine base image; move to 14.5.0

--- docker/install.sh
+++ docker/install.sh
@@ -1,23 +1,3 @@
 #!/bin/bash -Eeu
 
-apt-get update
-apt-get install --yes curl
-curl --silent --location https://deb.nodesource.com/setup_14.x | bash -
-apt-get install --yes nodejs
 npm install --global jshint
-
-npm install --global n
-# keep old version for backward compatibility with old Katas
-n 0.12.7
-n 4.1.1
-n 4.2.1
-n 6.11.1
-n 8.2.1
-n 8.4.0
-n 9.4.0
-n 9.10.1
-n 10.1.0
-n 13.7.0
-n 14.1.0
-
-chmod -R a+w /usr/local
